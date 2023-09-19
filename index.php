1eea8feadb1ef2048b8732235ab98ab5c6b17111
Ed Baker <edwbaker@gmail.com>
2023-09-19 10:34:15 +0000

    Show random audio segment

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 
   <body>
     <h1>Segments Demo</h1>
+    <?php
+      //Get list of wav files from data directory
+      $files = glob('data/*.wav');
+
+      //Select file at random
+      $file = $files[array_rand($files)];
+
+      //Create html audio element
+      echo "<audio controls autoplay>";
+      echo "<source src='$file' type='audio/wav'>";
+      echo "</audio>";
+
+
+    ?>
   </body>
 
 </html>
